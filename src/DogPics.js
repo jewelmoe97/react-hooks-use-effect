1eafd9f4f35ca9a91da7fd7acc5ea20a1bb16587
jewel <snowdecember079@gmail.com>
2022-09-05 11:11:19 -0400

    pass

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,29 +1,28 @@
-import React, { useState, useEffect } from "react";
+
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
+import React, { useState, useEffect } from 'react';
+
 function DogPics() {
-  const [images, setImages] = useState([]);
+   const [images, setImages] = useState([]);
 
-  useEffect(() => {
-    console.log("useEffect");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
-      .then((r) => r.json())
-      .then((data) => {
-        console.log("setState");
-        setImages(data.message);
-      });
-  });
+   useEffect(() => {
+      fetch('https://dog.ceo/api/breeds/image/random/3')
+         .then((r) => r.json())
+         .then((data) => {
+            // setting state in the useEffect callback
+            setImages(data.message);
+         });
+   });
 
-  console.log("render");
-
-  return (
-    <div>
-      {images.map((image) => (
-        <img src={image} key={image} />
-      ))}
-    </div>
-  );
+   return (
+      <div>
+         {images.map((image) => (
+            <img src={image} key={image} />
+         ))}
+      </div>
+   );
 }
 
 export default DogPics;
